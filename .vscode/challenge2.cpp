9ccf28b3281135b009138f2cdf59b2adb4f5cca5
Satya37x1112 <manoharisatyasarthak@gmail.com>
2025-10-16 06:59:19 +0530

    challenge2

---
+++ .vscode/challenge2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    string name;
+    cout<<"Enter your name: "<<endl;
+    cin>>name;
+    cout<<"Enter your age: "<<endl;
+    cin>>age;
+    cout<<"Hello "<<name<<" your age is: "<<age<<endl;
+    return 0;
+}
