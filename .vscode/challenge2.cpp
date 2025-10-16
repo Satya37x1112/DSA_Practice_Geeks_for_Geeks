f318c84e476c17bf854a3770551e7599948b3f72
Satya37x1112 <manoharisatyasarthak@gmail.com>
2025-10-16 07:11:29 +0530

    more_optimised

--- .vscode/challenge2.cpp
+++ .vscode/challenge2.cpp
@@ -1,12 +1,13 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int age;
+
+int main() {
     string name;
-    cout<<"Enter your name: "<<endl;
-    getline(cin,name);
-    cout<<"Enter your age: "<<endl;
-    cin>>age;
-    cout<<"Hello "<<name<<" your age is: "<<age<<endl;
+    int age;
+    cout << "Enter your name: " << endl;
+    getline(cin, name);
+    cout << "Enter your age: " << endl;
+    cin >> age;
+    cout << "Hello " << name << " your age is: " << age << endl;
     return 0;
 }
