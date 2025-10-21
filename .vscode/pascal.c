e029dd5c6c1828b5d0eeae4be6f3241144d678c4
Satya37x1112 <manoharisatyasarthak@gmail.com>
2025-10-21 20:49:47 +0530

    pascal_edited

--- .vscode/pascal.c
+++ .vscode/pascal.c
@@ -17,6 +17,9 @@ int main() {
     scanf("%d", &n);
 
     for (int i = 0; i < n; i++) {
+        for(int space = 0; space < n - i - 1; space++) {
+            printf(" ");
+        }
         for (int j = 0; j <= i; j++) {
             printf("%d ", combination(i, j));
         }
