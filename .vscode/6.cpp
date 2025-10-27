a821eca7531f73302ed2b7a0a964d1c7b14f2522
Satya37x1112 <manoharisatyasarthak@gmail.com>
2025-10-27 20:09:33 +0530

    function_prototyping

---
+++ .vscode/6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int sum(int,int);
+int main() {
+    int a,b;
+    cout<<"Enter first number: "<<endl;
+    cin>>a;
+    cout<<"Enter second number: "<<endl;
+    cin>>b;
+    cout<<"Sum is: "<<sum(a,b)<<endl;
+    return 0;
+}
+int sum(int x,int y){
+    return x+y;
+}
