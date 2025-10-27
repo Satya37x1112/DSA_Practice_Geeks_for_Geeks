2a1e1a8190be9bf49625f81bc9d4fcaba519e84d
Satya37x1112 <manoharisatyasarthak@gmail.com>
2025-10-27 06:33:38 +0530

    changes

--- .vscode/1.cpp
+++ .vscode/1.cpp
@@ -5,5 +5,9 @@ int main(){
     int* b=&a;
     cout<<"The address of a is: "<<b<<endl;
     cout<<"The value of a using pointer b is: "<<*b<<endl;
+    int** c;
+    c=&b;
+    cout<<"The address of pointer b is: "<<c<<endl;
+    cout<<"The value of a using double pointer c is: "<<**c<<endl;
     return 0;
 }
