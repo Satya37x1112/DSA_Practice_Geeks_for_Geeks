3fa50d7b193b2cb7259e32287ae617e1b22377c5
Satya37x1112 <manoharisatyasarthak@gmail.com>
2025-10-28 07:23:28 +0530

    classes

---
+++ .vscode/10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class hello 
+{
+    public: 
+        void display()
+        {   
+        cout<<"hello";
+        }
+};      
+
+int main() {
+    hello satya;
+    satya.display();
+    return 0;
+}
