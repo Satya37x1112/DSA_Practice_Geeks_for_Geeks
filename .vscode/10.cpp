f42b8142a9da6056ccedc8767c07484201b83017
Satya37x1112 <manoharisatyasarthak@gmail.com>
2025-10-29 06:55:47 +0530

    private

--- .vscode/10.cpp
+++ .vscode/10.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 using namespace std;
 
-class hello 
+class hello
 {
-    public: 
-        void display(void);
-};      
-void hello::display(void) 
+private:
+    int a;
+
+public:
+    void display(void);
+};
+void hello::display(void)
 {
+    cout<<"Enter the private number a: "<<endl;
+    cin>>a;
     cout << "Hello, Satya!" << endl;
 }
 
-int main() {
+int main()
+{
     hello satya;
     satya.display();
     return 0;
