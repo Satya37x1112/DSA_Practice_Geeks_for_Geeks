f71274d24716c6aadf5d8316d32ee95da990f50a
Satya37x1112 <manoharisatyasarthak@gmail.com>
2025-10-29 06:52:54 +0530

    public

--- .vscode/10.cpp
+++ .vscode/10.cpp
@@ -4,11 +4,12 @@ using namespace std;
 class hello 
 {
     public: 
-        void display()
-        {   
-           cout<<"hello";
-        }
+        void display(void);
 };      
+void hello::display(void) 
+{
+    cout << "Hello, Satya!" << endl;
+}
 
 int main() {
     hello satya;
