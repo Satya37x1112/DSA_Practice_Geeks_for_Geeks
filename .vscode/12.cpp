f1ea4606736fed671a6e8bf67ffd5d09c765f835
Satya37x1112 <manoharisatyasarthak@gmail.com>
2025-10-30 06:40:07 +0530

    firend

---
+++ .vscode/12.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class yo{
+    int a,b;
+public:
+    void sum(int n1,int n2){
+        a=n1;
+        b=n2;
+    }
+    void display(){
+        cout<<"The sum is "<<a+b<<endl;
+    }
+    friend yo friendsum(yo f1,yo f2);
+
+};
+yo friendsum(yo f1,yo f2){
+    yo f3;
+    f3.sum(f1.a,f2.b);
+    return f3;
+}
+int main() {
+    yo s1,s2,s3;
+    s1.sum(3,4);
+    s2.sum(5,6);
+    s3=friendsum(s1,s2);
+    s3.display();
+    return 0;
+}
