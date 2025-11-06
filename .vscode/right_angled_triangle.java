8cd83eea5c26fdb1b4a7de9842f4ae1c1fe0df6f
Satya37x1112 <manoharisatyasarthak@gmail.com>
2025-11-06 10:51:18 +0530

    lab

---
+++ .vscode/right_angled_triangle.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class right_angled_triangle{
+    public static void main(String args[]){
+        int a;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number of iterations of  ");
+        a=sc.nextInt();
+        for(int i=0;i<a;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
