89cfa49e9388d5f107f6ce98e0dd40fe3b05d360
Satya37x1112 <manoharisatyasarthak@gmail.com>
2025-11-06 11:02:13 +0530

    Fix right-angled triangle Java program - improved nested loops and user prompt

--- .vscode/right_angled_triangle.java
+++ .vscode/right_angled_triangle.java
@@ -3,7 +3,7 @@ class right_angled_triangle{
     public static void main(String args[]){
         int a;
         Scanner sc=new Scanner(System.in);
-        System.out.print("Enter the number of iterations of  ");
+        System.out.print("Enter the number of rows for the triangle: ");
         a=sc.nextInt();
         for(int i=0;i<a;i++)
         {
