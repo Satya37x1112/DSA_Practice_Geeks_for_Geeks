c7e2957aeccd9b460ac1d920630364bcdef982de
Satya37x1112 <manoharisatyasarthak@gmail.com>
2025-11-06 11:18:11 +0530

    updated

--- .vscode/right_angled_triangle.java
+++ .vscode/right_angled_triangle.java
@@ -3,12 +3,13 @@ class right_angled_triangle{
     public static void main(String args[]){
         int a;
         Scanner sc=new Scanner(System.in);
-        System.out.print("Enter the number of rows for the triangle: ");
+        System.out.print("Enter the number of iterations: ");
         a=sc.nextInt();
         for(int i=0;i<a;i++)
         {
             for(int j=0;j<=i;j++)
             {
+
                 System.out.print("*");
             }
             System.out.println();
